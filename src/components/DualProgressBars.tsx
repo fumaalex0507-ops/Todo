eeb82ee889f32a285fd8ef0c307dbed52c984c35
fumaalex0507-ops <fumaalex0507@gmail.com>
2026-08-03 15:51:09 +0900

    体重/期間の進捗バーに色分けを追加(体重=インディゴ、期間=オレンジ)

--- src/components/DualProgressBars.tsx
+++ src/components/DualProgressBars.tsx
@@ -11,14 +11,18 @@ export function DualProgressBars({ weightPercent, datePercent }: DualProgressBar
         <div className="progress-bar">
           <div className="progress-bar__fill" style={{ width: `${weightPercent}%` }} />
         </div>
-        <span className="dual-progress__percent">{Math.round(weightPercent)}%</span>
+        <span className="dual-progress__percent dual-progress__percent--weight">
+          {Math.round(weightPercent)}%
+        </span>
       </div>
       <div className="dual-progress__row">
         <span className="dual-progress__label">期間</span>
         <div className="progress-bar">
           <div className="progress-bar__fill progress-bar__fill--date" style={{ width: `${datePercent}%` }} />
         </div>
-        <span className="dual-progress__percent">{Math.round(datePercent)}%</span>
+        <span className="dual-progress__percent dual-progress__percent--date">
+          {Math.round(datePercent)}%
+        </span>
       </div>
     </div>
   )
