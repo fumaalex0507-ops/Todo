a81182cc33a7f3fc0e1eb04c238cf2d0561da71a
fumaalex0507-ops <fumaalex0507@gmail.com>
2026-08-03 22:57:24 +0900

    Service Workerのページ取得でHTTPキャッシュを無視するよう修正
    GitHub PagesがCache-Control: max-age=600を付与しているため、network-first
    のfetchでもブラウザのディスクキャッシュから古いHTMLが返り、アプリを
    開き直しても最新版が反映されないことがあった。fetchにcache: 'no-store'
    を指定してキャッシュを無視し、常に最新を取得するよう修正

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'todo-app-v2'
+const CACHE_NAME = 'todo-app-v3'
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
@@ -26,7 +26,7 @@ self.addEventListener('fetch', (event) => {
   // オフライン時のみキャッシュにフォールバックする。
   if (event.request.mode === 'navigate') {
     event.respondWith(
-      fetch(event.request)
+      fetch(event.request, { cache: 'no-store' })
         .then((response) => {
           const clone = response.clone()
           caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone))
